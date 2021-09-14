32369d4c2dc6eec10407a13b7fd5eebe239a82d5
Kaushik Sadashiva Reddy <kausredd@codeaurora.org>
2021-09-14 14:27:32 -0700

    Device config: unset radio rat on property
    This is not needed anymore as RAT info is
    passed in the results for network scan and
    is being set as part of the API
    setNetworkSelectionModeManual_1_5
    
    Change-Id: Ic12ac8f1f4f4b0a3acf81fcca65fcfe5b1e822a6

--- common.mk
+++ common.mk
@@ -28,7 +28,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.rat_on=combine \
     persist.backup.ntpServer=0.pool.ntp.org \
     sys.vendor.shutdown.waittime=500 \
     persist.vendor.radio.procedure_bytes=SKIP
